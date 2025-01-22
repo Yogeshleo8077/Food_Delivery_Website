2a54a1ae312fad09ea3cd54fcdc5a2e5a4b76dce
Yogesh Kushwah <130173369+Yogeshleo8077@users.noreply.github.com>
2025-01-22 14:45:57 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,6 +5,9 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
+export const url = 'https://food-delivery-website-qizm.onrender.com'
+
+
 export const assets = {
     logo,
     add_icon,
@@ -13,5 +16,3 @@ export const assets = {
     upload_area,
     parcel_icon
 }
-
-export const url = 'http://localhost:4000'
